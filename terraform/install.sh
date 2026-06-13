033eaad8203f38e38a009fd36cf7d9effa67c61f
aminatacissoko <aminataci20@gmail.com>
2026-06-13 19:45:12 +0000

    feat: ajout script install Jenkins et Docker

---
+++ terraform/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+apt-get update -y
+
+# Installation Java (requis pour Jenkins)
+apt-get install -y fontconfig openjdk-17-jre
+
+# Installation Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt-get update -y
+apt-get install -y jenkins
+
+# Installation Docker
+apt-get install -y ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+usermod -aG docker ubuntu
+usermod -aG docker jenkins
+
+systemctl enable jenkins
+systemctl start jenkins
+systemctl enable docker
+systemctl start docker
